9646bdcd70dc61b4133b032f3640183a1653c4fa
Finley Baker <finjothegreat@gmail.com>
2024-09-23 12:28:36 -0600

    Add underscore support

--- src/markdown.ts
+++ src/markdown.ts
@@ -4,14 +4,14 @@ export function parse(input: string): tokens.Token[] {
   return tokenize(input);
 }
 
-const EM_STRONG_CHARS = ["*", "_"];
+const EM_STRONG_CHARS: ReadonlySet<string> = new Set<string>(["*", "_"]);
 
 export function tokenize(input: string): tokens.Token[] {
   const currentTokens: tokens.Token[] = [];
   let currentText: string = "";
   for (const char of input) {
-    if (currentText === "*") {
-      if (char === "*") {
+    if (EM_STRONG_CHARS.has(currentText)) {
+      if (EM_STRONG_CHARS.has(char)) {
         currentTokens.push(tokens.boldToken());
         currentText = "";
       } else {
@@ -20,14 +20,14 @@ export function tokenize(input: string): tokens.Token[] {
       }
       continue;
     }
-    if (char === "*" && currentText !== "") {
+    if (EM_STRONG_CHARS.has(char) && currentText !== "") {
       currentTokens.push(tokens.textToken(currentText));
       currentText = "";
     }
     currentText += char;
   }
 
-  if (currentText === "*") {
+  if (EM_STRONG_CHARS.has(currentText)) {
     currentTokens.push(tokens.italicToken());
   } else if (currentText !== "") {
     currentTokens.push(tokens.textToken(currentText));
--- src/markdown_test.ts
+++ src/markdown_test.ts
@@ -15,10 +15,21 @@ describe("markdown", () => {
         tokens.textToken("asdf"),
         tokens.italicToken(),
       ]);
+
+      expect(tokenize("_asdf_")).toEqual([
+        tokens.italicToken(),
+        tokens.textToken("asdf"),
+        tokens.italicToken(),
+      ]);
     });
 
     it("tokenizes bold", () => {
       expect(tokenize("**asdf**")).toEqual([
+        tokens.boldToken(),
+        tokens.textToken("asdf"),
+        tokens.boldToken(),
+      ]);
+      expect(tokenize("__asdf__")).toEqual([
         tokens.boldToken(),
         tokens.textToken("asdf"),
         tokens.boldToken(),
